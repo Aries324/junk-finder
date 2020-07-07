beb372fb17c0dd71d4714e37cfe44f792e25d390
Tiffany McLean <tamclean324@gmail.com>
2020-07-07 11:15:29 -0400

    creates models and base skeleton

---
+++ junkapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from junkapp.models import ItemsPost, MyUser
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+admin.site.register(MyUser, UserAdmin)
+admin.site.register(ItemsPost)
+
---
+++ junkapp/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from phone_field import PhoneField
+from django.contrib.auth.models import AbstractUser
+
+
+
+# Create your models here.
+class MyUser(AbstractUser):
+    email = models.EmailField(max_length=200)
+    name = models.CharField(max_length=200)
+    phone = PhoneField(blank=True, help_text='Contact phone number')
+
+
+class ItemsPost(models.Model):
+    FURNITURE = ' F'
+    ELECTRONICS = 'E'
+    HOME_IMPROVEMENT = 'H'
+    SCRAPS = 'S'
+    CLOTHING = 'C'
+
+    ITEM_CHOICES = [
+        (FURNITURE, 'Furniture'),
+        (ELECTRONICS, 'Electronics'),
+        (HOME_IMPROVEMENT, 'Home_Improvement'),
+        (SCRAPS, 'Scraps'),
+        (CLOTHING, 'Clothing')
+
+    ]
+    claimed = models.BooleanField(default=False)
+    description = models.TextField()
+    title = models.CharField(max_length=200)
+    email = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    address = models.URLField()
+    items = models.CharField(max_length=20, choices=ITEM_CHOICES)
