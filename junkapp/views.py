bf20fb047466a22aa3a34cfb462ff3e93d5615d9
Safia Ali <safialiusa@gmail.com>
2020-07-09 11:48:28 +0600

    Signup, login & logout done with simple form

--- junkapp/views.py
+++ junkapp/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from junkapp.forms import LoginForm, SignUpForm
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect, reverse, HttpResponseRedirect
+from .models import MyUser
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def login_view(request):
+    html = "login.html"
+    form = None
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                username=data["username"], password=data["password"]
+            )
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(
+                    request.GET.get('next', reverse('home'))
+                )
+    else:
+        form = LoginForm()
+    return render(request, html, {"form": form})
+
+
+def signup(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            new_user = MyUser.objects.create_user(
+                name=data["name"],
+                username=data["username"],
+                email=data["email"],
+                phone=data["phone"],
+                password=data['password'])
+            new_user.set_password(raw_password=data['password'])
+            new_user.save()
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+@login_required(login_url='/login/')
+def home(request):
+    return render(request, 'home.html')
+
+
+def logout_action(request):
+    logout(request)
+    return redirect(request.GET.get("next", reverse('login')))
--- junkfinder/urls.py
+++ junkfinder/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from junkapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('signup/', views.signup),
+    path('logout/', views.logout_action, name='logout'),
+    path("login/", views.login_view, name="login"),
 ]
