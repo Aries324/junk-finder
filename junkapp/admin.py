568a146d9e96f55e21e5b962b9bc6d280d2af25b
Tiffany McLean <tamclean324@gmail.com>
2020-07-14 11:33:07 -0400

    changes phone field to char and adds custom user admin

--- junkapp/admin.py
+++ junkapp/admin.py
@@ -3,7 +3,12 @@ from junkapp.models import ItemsPost, MyUser
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = MyUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone',)}),
+    )
 
-admin.site.register(MyUser, UserAdmin)
+admin.site.register(MyUser, CustomUserAdmin)
 admin.site.register(ItemsPost)
 
